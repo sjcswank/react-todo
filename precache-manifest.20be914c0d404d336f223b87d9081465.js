cb913e05b468eb7d523bf16a12cf7a30397fa78a
sjcswank <sjcswank@gmail.com>
2019-04-27 17:54:41 -0500

    Updates

---
+++ precache-manifest.20be914c0d404d336f223b87d9081465.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "052fad23ca9f036fcf47",
+    "url": "/react-todo/static/js/runtime~main.79666361.js"
+  },
+  {
+    "revision": "abbd9310d8f640fec88a",
+    "url": "/react-todo/static/js/main.1fa7907a.chunk.js"
+  },
+  {
+    "revision": "bedea4066ca0672b7b81",
+    "url": "/react-todo/static/js/2.83b9a475.chunk.js"
+  },
+  {
+    "revision": "abbd9310d8f640fec88a",
+    "url": "/react-todo/static/css/main.a7f8eb79.chunk.css"
+  },
+  {
+    "revision": "86cee8d99a9e202ca908e91e00889c8d",
+    "url": "/react-todo/index.html"
+  }
+];
