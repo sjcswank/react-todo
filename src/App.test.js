5e211331ccba14181a11009ad89c41706744b232
sjcswank <sjcswank@gmail.com>
2025-09-21 17:56:03 -0500

    Added jest unit tests to App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -7,3 +7,35 @@ it('renders without crashing', () => {
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+it('adds a todo item', () => {
+  const div = document.createElement('div');
+  const app = ReactDOM.render(<App />, div);
+  const todo = { content: "Test Todo" };
+  app.addTodo(todo);
+  expect(app.state.todos.length).toBe(1);
+  expect(app.state.todos[0].content).toBe("Test Todo");
+});
+
+it('toggles the complete flag of a todo', () => {
+  const div = document.createElement('div');
+  const app = ReactDOM.render(<App />, div);
+  const todo = { content: "Test Todo" };
+  app.addTodo(todo)
+  app.toggleComplete(app.state.todos[0].id);
+  expect(app.state.todos[0].complete).toBe(true);
+  app.toggleComplete(app.state.todos[0].id);
+  expect(app.state.todos[0].complete).toBe(false);
+});
+
+it('deletes a todo', () => {
+  const div = document.createElement('div');
+  const app = ReactDOM.render(<App />, div);
+  let todo = { content: "Todo 1" };
+  app.addTodo(todo);
+  todo = { content: "Todo 2"};
+  app.addTodo(todo);
+  app.deleteTodo(app.state.todos[1].id);
+  expect(app.state.todos.length).toBe(1);
+  expect(app.state.todos[0].content).toBe("Todo 2");
+});
